ac21960d2f205f66c070d9aca3cb1c685b23e501
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2025-12-05 19:37:05 +0530

    solved problem 35 Add solution for 'Search Insert Position' problem
    Implemented a function to find the index to insert a target value into a sorted array, as per LeetCode problem 35. The function returns the index if the target is found, or the position where it should be inserted to maintain order.

---
+++ arrays/EASY/0035 Search Insert Position.py
@@ -0,0 +1,37 @@
+# 35. Search Insert Position
+
+# Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+# You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+# Example:
+# Input: nums = [1,3,5,6], target = 5
+# Output: 2
+
+# Example:
+# Input: nums = [1,3,5,6], target = 2
+# Output: 1
+
+# Example:
+# Input: nums = [1,3,5,6], target = 7
+# Output: 4
+
+nums = [1,3,5,6] 
+target = 5
+
+# nums = [-5, -2, 0, 3, 9]
+# target = 3
+
+
+def searchInsert(self, nums, target):
+    for i in range(len(nums)):
+        if nums[i] >= target:
+            return i
+    return len(nums)
+            
+        
+
+a=searchInsert(nums,target)
+print(a)
