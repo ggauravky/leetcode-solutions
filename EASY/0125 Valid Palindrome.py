202384389e6bf8ecb21b0d9b01f2cf6dadea86d2
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2026-01-10 17:30:18 +0530

    Add solution for 125. Valid Palindrome
    Implements a function to check if a given string is a palindrome by filtering out non-alphanumeric characters and comparing the cleaned string to its reverse.

---
+++ EASY/0125 Valid Palindrome.py
@@ -0,0 +1,39 @@
+# 125. Valid Palindrome
+
+# A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+# Given a string s, return true if it is a palindrome, or false otherwise.
+
+ 
+
+# Example 1:
+
+# Input: s = "A man, a plan, a canal: Panama"
+# Output: true
+# Explanation: "amanaplanacanalpanama" is a palindrome.
+# Example 2:
+
+# Input: s = "race a car"
+# Output: false
+# Explanation: "raceacar" is not a palindrome.
+# Example 3:
+
+# Input: s = " "
+# Output: true
+# Explanation: s is an empty string "" after removing non-alphanumeric characters.
+# Since an empty string reads the same forward and backward, it is a palindrome
+
+s = "0P"
+list=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9"]
+
+updated_s=""
+rev_updated_s=""
+for stri in s.lower():
+    if stri in list:
+        updated_s=updated_s+stri
+
+rev_updated_s=updated_s[::-1]
+if updated_s == rev_updated_s:
+    print(True)
+else:
+    print(False)
