89e0f1a78c5ede057027aab437684faa4ddeab2a
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2026-03-03 23:43:26 +0530

    Add commented example test case
    Append a commented-out example (nums = [1,3,5,6], target = 2) to EASY/0035 Search Insert Position.py for illustrative/testing purposes. This change adds example usage and does not affect runtime behavior.

--- EASY/0035 Search Insert Position.py
+++ EASY/0035 Search Insert Position.py
@@ -51,3 +51,11 @@ for i in range(len(nums)):
     if nums[i] >= target:
         print(i)
         break
+
+
+# nums = [1,3,5,6]
+# target = 2    
+# for i in range(len(nums)):
+#     if nums[i] >= target:
+#         print(i)
+#         break
