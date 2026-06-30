db80f755dede804a86b70a237dceecee7d529c17
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2026-06-30 23:18:18 +0530

    Update 0026 Remove Duplicates from Sorted Array.py

--- EASY/0026 Remove Duplicates from Sorted Array.py
+++ EASY/0026 Remove Duplicates from Sorted Array.py
@@ -36,6 +36,18 @@ class Solution(object):
                 write_index += 1
         
         return write_index
+    
+    # another approach
+    
+        i = 0  
+
+        for j in range(1, len(nums)):
+            if nums[j] != nums[i]:
+                i += 1
+                nums[i] = nums[j]
+
+        return i + 1
+    
 
 nums = [0,0,1,1,1,2,2,3,3,4]
 # nums = [1,1,2]
